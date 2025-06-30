a01bc87fab64b2add19ca1815b30a27751d970e6
pliu0157 <pliu0157@uni.sydney.edu.au>
2025-06-30 21:34:03 +1000

    add Cart html and cart page controller

---
+++ src/main/java/com/bookhub/controller/CartPageController.java
@@ -0,0 +1,37 @@
+// ===== CartPageController - Cart Page Controller =====
+// src/main/java/com/bookhub/controller/CartPageController.java
+package com.bookhub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for cart-related page routing
+ * Handles the display of cart page and other cart-related views
+ */
+@Controller
+public class CartPageController {
+
+    /**
+     * Display the shopping cart page
+     *
+     * @return the cart template name
+     */
+    @GetMapping("/cart")
+    public String cartPage() {
+        System.out.println("Cart page accessed"); // Debug log
+        return "cart";
+    }
+
+    /**
+     * Display the checkout page (placeholder for future implementation)
+     *
+     * @return the checkout template name
+     */
+    @GetMapping("/checkout")
+    public String checkoutPage() {
+        // TODO: Add authentication check
+        // TODO: Validate cart contents
+        return "checkout"; // Will be implemented later
+    }
+}
